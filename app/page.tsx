be496c3ec8a4f9ee69f9d04f6cdaabf8def8e663
ARPHAM <oaichua42@gmail.com>
2025-11-01 09:16:34 +0700

    navbar

--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,19 @@
 import { useState } from "react";
 import Table from "./table";
 import Link from "next/link";
+import Header from "./components/header";
+import NavbarMain from "./components/navbarMain";
 
 export default function Home() {
   return (
-    <main className="p-8 bg-gray-100 min-h-screen">
+    <main className="bg-gray-100 min-h-screen">
+      <Header />
+      <NavbarMain />
       <Link href="/Login" className="text-blue-500 hover:underline">
-        Go to Login
+        Login
+      </Link>
+      <Link href="/testcolor" className="ml-4 text-blue-500 hover:underline">
+        TestColor
       </Link>
     </main>
   );
