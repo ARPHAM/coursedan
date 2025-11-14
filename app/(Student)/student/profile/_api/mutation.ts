181a01308fb0ed621107fe3b42c1ee677ffb17ba
ARPHAM <oaichua42@gmail.com>
2025-11-14 17:07:22 +0700

    profile student

---
+++ app/(Student)/student/profile/_api/mutation.ts
@@ -0,0 +1,19 @@
+import { useMutation } from "@tanstack/react-query";
+import axios from "@/config/axios";
+
+export type Info = {
+  avatar: string | null;
+  email: string;
+  fullName: string;
+  id: string;
+  role: string | string[];
+};
+
+export const useInfoStudent = () => {
+  return useMutation<Info>({
+    mutationFn: async () => {
+      const { data } = await axios.get<Info>("/api/Student/info");
+      return data;
+    },
+  });
+};
